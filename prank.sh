7156e2573b0a0d2a27fbf36777d9bbb9af0abb37
farisunlike <49750385+farisunlike@users.noreply.github.com>
2019-04-21 18:34:34 +0700

    Add files via upload

---
+++ prank.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo 'install PRANK'
+      cd $HOME
+      pkg install git && pkg install php
+      pkg install git
+      git clone https://github.com/siputra12/prank
+      echo 'installing succes....! '
+      echo 'silakan kembali ke halaman cd'
+      echo 'lalu pilih cd prank'
+      sleep 3
